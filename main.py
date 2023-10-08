161c71be1671fe4895971e27bdaff3657182fb77
Bloodstone2179 <colinfrurze@gmail.com>
2023-10-08 21:50:16 +0100

    'hi'

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ b = int(input("Blue Amount: ")) | 123
 
 y = canvas(100, 100)
 y.WritePixel(1, 1, color_rgb(r, g, b))  # each byte = pixel color 255 colors   255 = red 
-print(color_rgb(r, g, b).code())
+print(color_rgb(r, g, 2).code())
 #y.WritePixel(100, 100,color_rgb(100, 0, 50).hexCode() )
 
 size = FileWriter.GetTheSizeOfImageInBytes(y) + (y.height * 4) + 54
